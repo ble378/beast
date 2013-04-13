979117d0f9024f4dff4ac6c0dd32dfa32c43c806
spazzoduck <dennisw@mit.edu>
2013-04-13 11:10:38 -0400

    all the things

--- js/script.js
+++ js/script.js
@@ -1,4 +1,77 @@
-console.log("HELLO");
+function createWanderingDiv() {
+    var img, seed, vx, vy, left, top, counter, interval;
+
+    var date = new Date();
+    seed = date.getSeconds()/60+date.getMinutes()/60; 
+
+    img = document.createElement('img');
+
+    img.src = "bee.png";
+
+    vx = seed*Math.random();
+    vy = seed*Math.random();
+    left = $(window).width()*Math.random();
+    top  = $(window).height()*Math.random();
+    img.style.position = "absolute";
+    img.style.left = left + "px";
+    img.style.top = top + "px";
+    img.style.width = "100px";  // Make these match the image...
+    img.style.height = "87px"; // ...or leave them out.
+    img.onclick = function() {
+      if (img.src.indexOf("bee")!=-1) {
+        img.src = "falafel.png";
+      } else {
+        $("#dinobarf").toggle();
+        $("#gemini").toggle();
+      }
+    }
+
+    document.body.appendChild(img);
+
+    counter = 100;
+    interval = 50; // ms
+    window.setTimeout(wanderAround, interval);
+
+    function wanderAround() {
+
+    --counter;
+    if (counter < 0)
+    {
+        // Done; remove it
+        document.body.removeChild(img);
+    }
+    else
+    {
+      // Animate a bit more
+        
+      vy += seed*Math.random()-0.5;
+      left += Math.floor(vy);
+      if (left < 0) {
+        vy = -vy*1.1;
+        left = 0;
+      }
+      if (left >= $(window).width()) {
+        vy = -vy*1.1;
+        left = $(window).width();
+      }
+      vx += seed*Math.random()-0.5;
+      top  += Math.floor(vx);
+      if (top < 0) {
+        vx = -vx*1.1;
+        top = 0;
+      }
+      if (top >= $(window).height()) {
+        vx = -vx*1.1;
+        top = $(window).height();
+      }
+      img.style.left = left + "px";
+      img.style.top  = top  + "px";
+
+      // Re-trigger ourselves
+      window.setTimeout(wanderAround, interval);
+    }
+  }
+}
 
 App = Ember.Application.create();
 
@@ -78,6 +151,67 @@ $(document).ready(function(){
   var timer = setInterval(App.info.tick, 1000*60);
   App.info.tick();
   App.bandView.appendTo("#bands");
+
+  $("#dinobarf").hover(
+    function() {this.src="dinobarf_purple.png";},
+    function() {this.src="dinobarf.png";}
+  );
+
+  $("#dinobarf").click(function() {
+    $("#gremlin1").toggle('slow');
+    $("#preg").hide();
+    $("#pregexplosion").hide();
+    $("#pregbaby").hide();
+    $("#notpreg").hide();
+    $("#baby").hide();
+    $("body").css("background-image","");
+  });
+
+  $("#gremlin1").hover(function() {
+    $("#gremlin2").toggle();
+  });
+
+  $("#gremlin1").click(function() {
+    $("#preg").toggle();
+    $("#gremlin1").toggle();
+  });
+
+  $("#preg").click(function(){
+    $("#preg").toggle();
+    $("#pregexplosion").toggle();
+  });
+
+  $("#pregexplosion").click(function(){
+    $("#pregexplosion").toggle();
+    $("#pregbaby").toggle();
+  });
+
+  $("#pregbaby").click(function(){
+    $("#pregbaby").toggle();
+    $("#notpreg").toggle();
+    $("#baby").toggle();
+  });
+
+  $("#notpreg").click(function(){
+    $("#notpreg").toggle();
+    $("#preg").toggle();
+    $("#baby").toggle();
+  });
+
+  $("#baby").click(function(){
+    $("#baby").toggle();
+    $("#notpreg").toggle();
+    $("#gremlin2").toggle();
+  });
+
+  $("#gremlin2").click(function(){
+    createWanderingDiv();
+  }); 
+
+  $("#gemini").click(function(){
+    $("body").css("background-image","url('squirrel.png')");
+  });
+
 });
 
 // App.bands[0].get('prettyTime');
